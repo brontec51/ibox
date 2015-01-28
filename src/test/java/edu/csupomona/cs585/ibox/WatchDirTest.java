2890215aae57063bc2977667b22ad51afeff4c8a
brontec51 <exdrk100@gmail.com>
2015-01-28 09:41:18 -0800

    Update.

--- src/test/java/edu/csupomona/cs585/ibox/WatchDirTest.java
+++ src/test/java/edu/csupomona/cs585/ibox/WatchDirTest.java
@@ -1,27 +1,39 @@
 package edu.csupomona.cs585.ibox;
 
-import static org.junit.Assert.assertTrue;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-import java.nio.file.WatchEvent;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.WatchService;
 
 import org.junit.Test;
 
+import edu.csupomona.cs585.ibox.sync.FileSyncManager;
+
 public class WatchDirTest {
+
+	private Path mPath = mock(Path.class);
+
+	private FileSyncManager mfsm = mock(FileSyncManager.class);
 
 	private WatchDir wd;
 
-	private boolean flag;
+	@Test
+	public void testWatchDir() throws IOException {
 
-	public WatchDirTest() {
-		wd = mock(WatchDir.class);
+		wd = new WatchDir(mPath, mfsm);
+
+		verify(mPath).register(isA(WatchService.class), ENTRY_CREATE,
+				ENTRY_DELETE, ENTRY_MODIFY);
 	}
 
 	@Test
-	public void testCast() {
-
-		flag = wd.cast(mock(WatchEvent.class)) instanceof WatchEvent;
-
-		assertTrue("cast(WatchEvent<T>) has failed.", flag);
+	public void testProcessEvent() {
+		wd.processEvents();
 	}
 }
